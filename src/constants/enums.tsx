9687646cbd0fe727f07eca7ff59e6ee6e7821c29
Liam1809 <hkhanhlam99@gmail.com>
2022-10-06 07:18:43 +0100

    add constants enum

---
+++ src/constants/enums.tsx
@@ -0,0 +1,12 @@
+export enum BUTTON_TYPE_CLASSES {
+  add = 'add',
+  remove = 'remove',
+  complete = 'complete',
+}
+
+export enum TODOS_ACTION_TYPE {
+  get = 'get',
+  add = 'add',
+  delete = 'delete',
+  complete = 'complete',
+}
